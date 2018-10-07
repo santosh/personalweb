8237e45be2a05ade63bc3940bc69ac047481a73f
Santosh Kumar <sntshkmr60@gmail.com>
2018-10-07 17:13:48 +0530

    user login functional (not logout)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    re_path(r'^signup/$', views.signup_view, name="signup")
+    re_path(r'^signup/$', views.signup_view, name="signup"),
+    re_path(r'^login/$', views.login_view, name="login")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # log the user in
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
 
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect("articles:list")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {"form": form})
