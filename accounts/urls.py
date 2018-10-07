219c55811667407c1f92a00d4cc851c98183cdfd
Santosh Kumar <sntshkmr60@gmail.com>
2018-10-07 17:22:41 +0530

    logout functional

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     re_path(r'^signup/$', views.signup_view, name="signup"),
-    re_path(r'^login/$', views.login_view, name="login")
+    re_path(r'^login/$', views.login_view, name="login"),
+    re_path(r'^logout/$', views.logout_view, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 
 def signup_view(request):
@@ -29,3 +29,9 @@ def login_view(request):
         form = AuthenticationForm()
 
     return render(request, 'accounts/login.html', {"form": form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+    return redirect("articles:list")
