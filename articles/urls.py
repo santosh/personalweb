6da18de7dd5c6c26cdd5cc5ee3b03b375560a1f7
Santosh Kumar <sntshkmr60@gmail.com>
2018-10-07 17:42:12 +0530

    protecting article_create view with login

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ app_name = "articles"
 
 urlpatterns = [
     re_path(r'^$', views.article_list, name="list"),
+    re_path(r'^create/$', views.article_create, name="create"),
     re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="details")
 ]
