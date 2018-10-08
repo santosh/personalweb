01ef873cf682c42f789285fbe1163ef7ead4d74c
Santosh Kumar <sntshkmr60@gmail.com>
2018-10-09 00:06:31 +0530

    automatic slugs

---
+++ assets/slugify.js
@@ -0,0 +1,15 @@
+const titleInput = document.querySelector("input[name=title]");
+const slugInput = document.querySelector("input[name=slug]");
+
+const slugify = val => {
+  return val
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/&/g, "-and-") // replace & with '-and-
+    .replace(/[\s\W-]+/g, "-"); // replace spaces with '-'
+};
+
+titleInput.addEventListener("keyup", e => {
+  slugInput.setAttribute("value", slugify(titleInput.value));
+});
