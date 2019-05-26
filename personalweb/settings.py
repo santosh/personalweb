ee413101592f87999f778180433fbb05f77ec911
Santosh Kumar <sntshkmr60@gmail.com>
2019-05-26 09:46:14 +0530

    Fixes static file hosting

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
